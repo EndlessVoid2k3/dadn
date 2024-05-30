6f3e100c9d61a696ae179deedf17ca32b35709e6
EndlessVoid2k3 <149134345+EndlessVoid2k3@users.noreply.github.com>
2024-05-30 21:45:57 +0700

    Add files via upload

---
+++ FanCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Card, CardMedia, CardContent, Typography} from '@mui/material';
+import InputSlider from './InputSlider';
+
+function FanCard() {
+    return (
+        <Box sx={{ display: 'flex', justifyItems: 'center', alignItems: 'center', marginBottom: '100px'}}>
+            <Card sx={{ width: 345, borderRadius: '16px', marginLeft: '20px'}}>
+                <CardMedia
+                    sx={{ height: 140}}
+                    image={'https://www.ushafans.com/sites/default/files/MicrosoftTeams-image%20%288%29.png'}
+                    title={'fan'}
+                />
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Fan
+                    </Typography>
+                    <InputSlider/>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+}
+
+export default FanCard;
---
+++ LampCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Card, CardMedia, CardContent, Typography} from '@mui/material';
+import Toggle from './Toggle';
+
+function LampCard() {
+    return (
+        <Box sx={{ display: 'flex', justifyItems: 'center', alignItems: 'center', marginBottom: '100px'}}>
+            <Card sx={{ width: 345, borderRadius: '16px', marginLeft: '20px'}}>
+                <CardMedia
+                    sx={{ height: 140}}
+                    image={'https://cdn.britannica.com/88/212888-050-6795342C/study-lamp-electrical-cord.jpg'}
+                    title={'Lamp'}
+                />
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lamp
+                    </Typography>
+                    <Toggle />
+                </CardContent>
+            </Card>
+        </Box>
+    );
+}
+
+export default LampCard;
